2017925316c6e5e0beaff098ab940380acf53333
Kenshi Shiode <atemu1212@yahoo.co.jp>
2021-07-10 21:34:40 +0900

    Remove updatePlaylist

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,7 +3,6 @@ module Types
     field :update_playlist_title, mutation: Mutations::UpdatePlaylistTitle
     field :update_playlist_default_bpm, mutation: Mutations::UpdatePlaylistDefaultBpm
     field :create_videos, mutation: Mutations::CreateVideos
-    field :update_playlist, mutation: Mutations::UpdatePlaylist
     field :update_video, mutation: Mutations::UpdateVideo
     field :create_video, mutation: Mutations::CreateVideo
     field :create_playlist, mutation: Mutations::CreatePlaylist
